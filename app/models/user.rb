71f86cb1b0ddd4e40c3f92b5763df88113559780
Pedro Silva <Pedro@cartas-mac.lan>
2010-12-18 18:41:28 +0000

    User signup complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,6 +35,12 @@ validates :password, :presence     => true,
    encrypted_password == encrypt(submitted_password)
  end
 
+ def self.authenticate(email, submitted_password)
+   user = find_by_email(email)
+   return nil  if user.nil?
+   return user if user.has_password?(submitted_password)
+ end
+
  private
 
    def encrypt_password
--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module SampleApp
   class Application < Rails::Application
+    
+    config.filter_parameters += [:password]
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
